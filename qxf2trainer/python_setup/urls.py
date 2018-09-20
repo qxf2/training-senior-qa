178feca072413bd9f82eba60d9f6620c2420906b
Qxf2 <mak@qxf2.com>
2018-09-20 16:22:06 +0530

    Finished the first prototype

--- qxf2trainer/python_setup/urls.py
+++ qxf2trainer/python_setup/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('before-you-begin', views.beforeyoubegin, name='beforeyoubegin'),
     path('what-to-expect', views.whattoexpect, name='whattoexpect'),
+    path('tutorial', views.install_python, name='tutorial'),
+    path('did-you-complete', views.didyoucomplete, name='didyoucomplete'),
+    path('once-you-complete',views.onceyoucomplete, name='onceyoucomplete'),
 ]
