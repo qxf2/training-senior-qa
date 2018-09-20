a35e9cb0d7461a6e8a28c517f025a02c99bb295d
Raji Gali <raji@qxf2.com>
2018-09-20 14:51:24 +0530

    home page and logout link added

--- qxf2trainer/qxf2trainer/urls.py
+++ qxf2trainer/qxf2trainer/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('qxf2trainer/', include('django.contrib.auth.urls'))
 ]
