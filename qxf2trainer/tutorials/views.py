66c247e574c31528cd87795f7ab7ec825207f466
Qxf2 <mak@qxf2.com>
2018-09-21 02:37:26 +0530

    Added images to login and styled the tutorials pag

--- qxf2trainer/tutorials/views.py
+++ qxf2trainer/tutorials/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.template import loader
 
 # Create your views here.
 @login_required
 def index(request):
-    return HttpResponse("This is the first page.<br><br><h3>SETUP</h3><p>In this section, you will be guided to setup your environment. Proceed in order. Click on the first link/task that you are yet to complete.</p><ol><li><a href=\"../python-setup/before-you-begin\">Setup Python</a></li></ol><br><br><h3>EXERCISES</h3><p>To be filled as we go through the hackathon</p>")
+    template = loader.get_template('tutorials/index.html')
+    return HttpResponse(template.render())
