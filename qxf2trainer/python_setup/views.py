650af3a40b2d0881476944820f43d614079926c3
Qxf2 <mak@qxf2.com>
2018-09-21 04:06:13 +0530

    Added styling for the tutorial page

--- qxf2trainer/python_setup/views.py
+++ qxf2trainer/python_setup/views.py
@@ -16,11 +16,8 @@ def whattoexpect(request):
 
 @login_required
 def install_python(request):
-    return HttpResponse("<p>We expect this exercise to take you somewhere between 15-minutes to 2 hours. If you take more than two hours, please post what you tried along wtih error messages to our slack channel.</p><p>Python has two versions (3.x and 2.7.x) and multiple distributions (IronPython, Conda, etc.). Python 3.x is the latest and ideally what we would all use. We'd love to recommend you use Python 3.x. However, for this tutorial, you are going to install the latest Python 2.7 version from the official site. We are doing this because we find many QA departments have not yet embraced Python 3.x  and we want these tutorials to be as close to your real work as possible. In keeping with our approach of helping you think in different ways, we have three approaches that work. There is no shame or pride in choosing anyone of them. Just choose the method you are comfortable with and proceed.</p><br>\
-    <h3>Approach 1: Try it out yourself</h3><ol><li>We'd like to promote independent thinking and problem solving. So if you feel ready, try to install Python without referring to any documentation</li></ol><br>\
-    <h3>Approach 2: Google for instructions</h3><ol><li>Google for Python 2.7 install <your operating system name></li><li>Follow the instructions</li></ol><br>\
-    <h3>Approach 3: follow this guide</h3><ol><li>Visit <a href=\"https://www.python.org/downloads/\">https://www.python.org/downloads/</a></li><li>Ignore the 3.x versions and click on the latest 2.7.x release</li><li>On the release page, choose Python 2.7 release for your operating system </li><li>Download the executable and follow the steps</li></ol><br>\
-    <h3>Exercise</h3><p>Once you are done, add the Python executable to your path environment variable. Hint: Google for 'add Python to path environment variable <your os name>'.</p><h3>Verify your setup</h3><p>After you are done with the exercise listed above, it is time to verify your Python installation. It is a good habit to take small steps and verify them immediately. This will help you avoid getting stuck later on. To verify your setup, pull up your terminal prompt and type python --version. You should see the version you installed listed.</p><br><a href=\"did-you-complete\">Next</a>")
+    template = loader.get_template('python_setup/tutorial.html')
+    return HttpResponse(template.render())
 
 @login_required
 def didyoucomplete(request):
