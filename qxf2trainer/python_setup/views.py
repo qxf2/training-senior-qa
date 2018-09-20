dd5d6eb17e383e080665108d1bdb373ca42452ee
Qxf2 <mak@qxf2.com>
2018-09-21 04:56:12 +0530

    Updated titles of python setup pages and styled the once you complete page

--- qxf2trainer/python_setup/views.py
+++ qxf2trainer/python_setup/views.py
@@ -26,4 +26,5 @@ def didyoucomplete(request):
 
 @login_required
 def onceyoucomplete(request):
-    return HttpResponse("<p>Now that you are done with this setup exercise, here is a habit we would like you to pick - start using Stack Overflow when you have a problem that you cannot solve.</p><h3>Stack Overflow</h3> <strong>Fact:</strong>All developers run into problems with their code every single day. But they have a very nice resource called <a href=\"https://stackoverflow.com\">Stack Overflow</a>. Stack Overflow is a question answer site for developers. Almost any issue you hit through this series of tutorials would have already been asked and solved on Stack Overflow. From now on, when you Google for a technical question, lookout for Stack Overflow links and learn to try out the first or second accepted answer.</p><br><a href=\"../tutorials\">Back to tutorial list</a>")
+    template = loader.get_template('python_setup/once-you-complete.html')
+    return HttpResponse(template.render())
